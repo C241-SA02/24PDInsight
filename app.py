d5a895bd24be2d4d1b355245aa326cb0a4c16090
PaceKW <kevrat21@gmail.com>
2024-05-29 03:17:22 +0700

    test: thunder client for testing

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
 from source.transcribe import transcribe_youtube_audio, transcribe_mp3_audio
+from wordcloud import WordCloud
 import re
+import io
+from collections import Counter
 
 app = Flask(__name__)
 
@@ -24,6 +27,37 @@ def index():
 def transcribe():
     try:
         # Request the url link that pass from the backend
+        url = request.args.get('url')
+        
+        # For Test URL from BUCKET
+        # url = 'https://storage.googleapis.com/files-bucket-24pdinsight/gibran.mp3'
+        
+        # For Test URL from YOUTUBE
+        # url = 'https://youtu.be/OdptPKaEMFQ?si=nf8e1Jyq9bBLfpL7'
+        
+        if not url:
+            return jsonify({'error': 'No URL provided'}), 400
+        
+        if youtube_regex.match(url):
+            # Transcribe audio from YouTube URL
+            transcription = transcribe_youtube_audio(url)
+        elif mp3_regex.match(url):
+            # Transcribe audio from MP3 URL
+            transcription = transcribe_mp3_audio(url)
+        else:
+            return jsonify({'error': 'Invalid URL format'}), 400
+        
+        # Return the transcription result
+        return jsonify({'transcription': transcription})
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+# Wordcloud
+@app.route('/wordcloud', methods=['GET'])
+def wordcloud():
+    try:
+       # Request the url link that pass from the backend
         # url = request.args.get('url')
         
         # For Test URL from BUCKET
@@ -44,9 +78,15 @@ def transcribe():
         else:
             return jsonify({'error': 'Invalid URL format'}), 400
         
-        # Return the transcription result
-        return jsonify({'transcription': transcription})
-    
+        # Generate word frequencies
+        wordcloud = WordCloud().process_text(transcription)
+        
+        # Convert to desired format with word key before size key
+        wordcloud_data = [{'word': word, 'size': str(frequency)} for word, frequency in wordcloud.items()]
+
+        # Return the word cloud data as JSON
+        return jsonify({'wordcloud': wordcloud_data})
+
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 

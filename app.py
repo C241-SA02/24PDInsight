8b644bd3042d470ebf822f3189e1b1dbc84183a5
PaceKW <kevrat21@gmail.com>
2024-05-28 20:41:22 +0700

    create: app.py + flask

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from flask import Flask, jsonify
 
 
-app = flask(__name__)
+app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return "Hello World"
+
+# Line for Check the code
+if __name__ == '__main__':
+    app.run(debug=True)
